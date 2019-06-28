428e611ef1d98aec95a8da1a072944be9f6739ca
warpdesign <nicolas.ramz@gmail.com>
2019-06-28 11:27:21 +0200

    ADDED: XCode project

---
+++ crossarts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  crossarts
+//
+//  Created by Nico on 28/06/2019.
+//  Copyright © 2019 Nico. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
