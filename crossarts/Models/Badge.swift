ed135593a671af15d1028953b3142543599e4390
Florent Heyraud <florentheyraud.pro@gmail.com>
2019-06-29 22:59:52 +0200

    feature-model-badge
    Modèle des badges

---
+++ crossarts/Models/Badge.swift
@@ -0,0 +1,20 @@
+//
+//  Badge.swift
+//  crossarts
+//
+//  Created by Imac FH on 29/06/2019.
+//  Copyright © 2019 Nico. All rights reserved.
+//
+
+import Foundation
+
+struct Badge {
+    let id: Int,
+    let categoryId: Int,
+    let name: String,
+    let subtitle: String,
+    let obtained: Int,
+    let minimumViews: Int,
+    let views: Int,
+    let thresholds: Array<Int>
+}
