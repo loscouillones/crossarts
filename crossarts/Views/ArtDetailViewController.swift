6a113c2557d36378875d74eb0ed4c58806d22501
warpdesign <nicolas.ramz@gmail.com>
2019-07-04 13:38:17 +0200

    ADDED: debug extension IMPROVED: more work on details page

--- crossarts/Views/ArtDetailViewController.swift
+++ crossarts/Views/ArtDetailViewController.swift
@@ -11,14 +11,77 @@ import UIKit
 class ArtDetailViewController: UIViewController {
     var artwork:Artwork?
     
+    
+    @IBOutlet weak var artLabel: UILabel!
+    
+    @IBOutlet weak var artImageView: UIImageView!
+    
+    
+    @IBOutlet weak var artDescription: UITextView!
+    @IBOutlet weak var relatedImageView1: UIImageView!
+    
+    
+    @IBOutlet weak var relatedLabel1: UILabel!
+    
+    
+    @IBOutlet weak var relatedImageView2: UIImageView!
+    
+    @IBOutlet weak var relatedLabel2: UILabel!
+    
+    @IBOutlet weak var relatedImageView3: UIImageView!
+    
+    @IBOutlet weak var relatedLabel3: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "detail"
+        
+        
         // Do any additional setup after loading the view.
+    }
+    
+    func updateUI() {
+        if artwork != nil {
+            artLabel.text = artwork?.title
+            artDescription.text = artwork?.description
+            artImageView.download(from: artwork!.landscapeUrl)
+            
+            // rounded corners
+            artImageView.layer.cornerRadius = 13
+            artImageView.layer.masksToBounds = true
+            
+            // related 1
+            if artwork!.related.count > 0 {
+                let relatedId = artwork!.related[0]
+                let relatedArtwork = Artwork.getArtwork(id: relatedId)
+                relatedLabel1.text = relatedArtwork?.title
+                relatedImageView1.download(from: relatedArtwork!.thumbUrl)
+            }
+            
+            // related 2
+            if artwork!.related.count > 1 {
+                let relatedId = artwork!.related[1]
+                let relatedArtwork = Artwork.getArtwork(id: relatedId)
+                relatedLabel2.text = relatedArtwork?.title
+                relatedImageView2.download(from: relatedArtwork!.thumbUrl)
+            }
+            
+            // related 3
+            if artwork!.related.count > 2 {
+                let relatedId = artwork!.related[2]
+                let relatedArtwork = Artwork.getArtwork(id: relatedId)
+                relatedLabel3.text = relatedArtwork?.title
+                relatedImageView3.download(from: relatedArtwork!.thumbUrl)
+            }
+        } else {
+            artLabel.text = nil
+            artDescription.text = nil
+            artImageView.image = nil
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        updateUI()
     }
     /*
     // MARK: - Navigation
